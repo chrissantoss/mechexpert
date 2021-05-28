cc850ebbe687d364db3e59dfb37e6c788ca2aa7d
Christopher Santos <51184477+chrissantoss@users.noreply.github.com>
2021-05-27 23:59:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Typography, Card, CardContent, CardMedia, Tabs, Tab, Button, AppBar, CssBaseline, Grid, Toolbar, Container, makeStyles  } from '@material-ui/core'
+import BuildIcon from '@material-ui/icons/Build';
+import landing from './pages/landing'
+import Testimonials from './components /Testimonials';
+
+const useStyles = makeStyles((theme) => ({
+
+}))
+const App = () => {
+    return (
+        <h1>
+            hello
+        </h1>
+    );
+}
+
+export default App;
---
+++ src/pages/landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Typography, Tabs, Tab, Button, AppBar, CssBaseline, Grid, Toolbar, Container, makeStyles  } from '@material-ui/core'
+
+function Copyright() {
+    return (
+        <Typography variant='body1'>
+            {'Copyright © '}
+        <Typography variant='body1'>MechExpert</Typography>
+        {new Date().getFullYear()}
+        {'.'}
+        </Typography>
+    )
+}
+export default function landing() {
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}

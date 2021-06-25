ad842ae00e108d57fb7f61d80cc0d1c3cfc69021
Christopher Santos <51184477+chrissantoss@users.noreply.github.com>
2021-06-25 00:12:26 -0400

    changes to intro description

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Typography, Card, CardContent, CardMedia, Tabs, Tab, Button, AppBar, CssBaseline, Grid, Toolbar, Container, makeStyles  } from '@material-ui/core'
+import { Typography, Card, CardContent, CardMedia, Tabs, Tab, Button, AppBar, CssBaseline, Grid, Toolbar, Container, makeStyles } from '@material-ui/core'
 import BuildIcon from '@material-ui/icons/Build';
 import LandingHeader from './components /LandingHeader';
+import IntroSection from './components /IntroSection';
 
 
 
 const App = () => {
-    
+
     return (
-        <LandingHeader />
+        <div>
+            <LandingHeader />
+            <IntroSection />
+        </div>
+
     );
 }
 
---
+++ src/components /IntroSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    introTitle: {
+        fontFamily: 'Semi Bold',
+        fontSize: 40,
+    },
+    introDescription: {
+        fontFamily: 'Inter',
+        fontSize: 14,
+    }
+}));
+
+export default function IntroSection() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Typography className={classes.introTitle}>Learn how to solve various mechanical engineering problems</Typography>
+            <Typography className={classes.introDescription}>MechExpert provides you with the tools to help you succeed while strengthening your skills</Typography>
+        </div>
+    )
+}
